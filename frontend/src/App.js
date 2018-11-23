100ec50f41c67178dcda49fbebd5a3342efcde7f
René <rene.bl89@gmail.com>
2018-11-23 20:51:18 +0100

    Added new function component for private routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import ChatRoom from './chatroom/ChatRoom';
-import { isAuthenticated } from './util/AuthManager';
+import * as AuthManager from './util/AuthManager';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Authentication from './auth/Authentication';
+import PrivateRoute from './util/PrivateRoute';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-	}
 
 	render() {
 		return (
@@ -16,9 +14,9 @@ class App extends Component {
 				<div className="App">
 					<Route path="/login" component={Authentication} />
 					<Route path="/register" component={Authentication} />
-					<Route path="/chatroom" component={ChatRoom} />
+					<PrivateRoute path="/chatroom" component={ChatRoom} />
 					<Route exact path="/"
-						render={() => isAuthenticated() ?
+						render={() => AuthManager.isAuthenticated() ?
 							<Redirect to="/chatroom" />
 							: <Redirect to="/login" />} />
 				</div>
